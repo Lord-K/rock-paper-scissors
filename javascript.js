9800c00bdc9e7f56b608e17e7af6fecfc1080642
Lord-K <m.karakalli@gmail.com>
2022-11-09 22:11:28 +0300

    showing results in the console

--- javascript.js
+++ javascript.js
@@ -1,39 +1,45 @@
+let options = ['rock', 'paper', 'scissors'];
+
 function getComputerChoice() {
-    let options = ['rock', 'paper', 'scissors'];
     let rand = Math.floor(Math.random() * options.length);
     let pcChoice = options[rand];
     return pcChoice;
-      
+
 }
 
-
-function playRound(playerSelection, computerSelection) {
+function playRound() {
+    let playerSelection = prompt('Rock, Paper or Scissors').toLowerCase();
+    let computerSelection = getComputerChoice();
 if(playerSelection === computerSelection) {
     return 'It is a draw!';
 } else if(playerSelection == 'paper' && computerSelection == 'rock') {
     return 'You win!';
 } else if(playerSelection == 'paper' && computerSelection =='scissors') {
-    return 'The computer wins!';
+    return 'Computer wins!';
 } else if(playerSelection == 'rock' && computerSelection == 'scissors') {
-    return 'You win';
+    return 'You win!';
 } else if(playerSelection =='rock' && computerSelection == 'paper') {
-    return 'The computer wins!';
+    return 'Computer wins!';
 } else if(playerSelection == 'scissors' && computerSelection == 'paper') {
     return 'You win!';
 } else if(playerSelection == 'scissors' && computerSelection == 'rock') {
-    return 'The computer wins!';
+    return 'Computer wins!';
+}else {
+    return 'Not a valid value'
 }
 }
 
-let playerSelection = prompt('Rock, Paper or Scissors').toLowerCase();
-let computerSelection = getComputerChoice();
+function log() {
+    let pcCount = 0;
+    let youCount = 0;
+
+
+}
 
 function gameOn() {
-    for(let i = 0; i < 5; i++) {
-    playRound([i]);
+    for(let i = 1; i < 6; i++) {
+     console.log(`Round ${i}` + " " + playRound(i));
 
     };
 }
 gameOn();
-    
-document.write(playRound(playerSelection, computerSelection));
